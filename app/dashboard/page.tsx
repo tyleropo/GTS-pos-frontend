8eaa2a6f8705c635ae5decaca9b3600afa3976e3
paikwat <tyleropo2004@gmail.com>
2025-05-14 23:56:59 +0800

    feat: add fullcalendar in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from "@/components/app-sidebar"
+import Calendar from "@/components/calendar"
 import { SectionCards } from "@/components/section-cards"
 import { SiteHeader } from "@/components/site-header"
 import {
@@ -25,6 +26,7 @@ export default function Page() {
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards />
               <div className="px-4 lg:px-6">
+                <Calendar/>
                 {/* <ChartAreaInteractive /> */}
               </div>
               {/* <DataTable data={data} /> */}
