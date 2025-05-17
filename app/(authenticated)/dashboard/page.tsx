8e329ae28d3a59a2eb12a9f2b99d11595a2b7ce2
paikwat <tyleropo2004@gmail.com>
2025-05-17 23:59:00 +0800

    refactor: make the siteheader dynamic and changes on each page

--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -13,7 +13,7 @@ import { Wrench } from "lucide-react";
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col">
-      <SiteHeader />
+      <SiteHeader title="Dashboard" />
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
           <SectionCards />
