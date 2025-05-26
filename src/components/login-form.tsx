d58ef6dff01f717860a1d8474404d041c0c305b0
paikwat <tyleropo2004@gmail.com>
2025-05-26 22:20:38 +0800

    feat: add authentication
    connect login/register frontend to backend

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,18 +1,46 @@
+"use client"
 import { cn } from "@/src/lib/utils"
 import { Button } from "@/src/components/ui/button"
 import { Card, CardContent } from "@/src/components/ui/card"
 import { Input } from "@/src/components/ui/input"
 import { Label } from "@/src/components/ui/label"
+import { useState } from "react"
+import { useRouter } from "next/navigation"
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const router =useRouter()
+  const [email,setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+
+    try{
+      const response = await fetch("http://localhost:8000/api/login",{
+        method:"POST",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify({email,password}),
+      })
+      const data = await response.json()
+
+      if (!response.ok) throw new Error(data.message || "Login Failed")
+      localStorage.setItem("token",data.access_token)
+      alert("Login Successful!")
+      router.push("/dashboard")
+    }
+    catch(err: any){
+      setError(err.message)
+    }   
+  }
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" onSubmit={handleSubmit}>
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
                 <h1 className="text-2xl font-bold">Welcome back</h1>
@@ -20,6 +48,9 @@ export function LoginForm({
                   Login to your GTS Mktg account
                 </p>
               </div>
+               {error && (
+                <div className="text-sm text-red-500 text-center">{error}</div>
+              )}
               <div className="grid gap-3">
                 <Label htmlFor="email">Email</Label>
                 <Input
@@ -27,6 +58,8 @@ export function LoginForm({
                   type="email"
                   placeholder="m@example.com"
                   required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
               <div className="grid gap-3">
@@ -39,7 +72,13 @@ export function LoginForm({
                     Forgot your password?
                   </a>
                 </div>
-                <Input id="password" type="password" required />
+                <Input 
+                 id="password"
+                 type="password"
+                 required
+                 value={password}
+                 onChange={(e) => setPassword(e.target.value)}
+                  />
               </div>
               <Button type="submit" className="w-full">
                 Login
